5865401d39ee99136b6c0308076613df91c2fd85
Lakshit Sharma <lakshit040@gmail.com>
2024-06-27 10:00:50 +0530

    Fixed scroll bug in Mobile Nav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { ScrollArea } from "./ui/scroll-area";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
@@ -16,36 +17,38 @@ const links = [
 const MobileNav = () => {
   const pathname = usePathname();
   return (
-    <Sheet>
+    <Sheet className="h-screen overflow-y-auto">
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
-        {/* logo */}
-        <div className="mt-32 mb-40 text-center text-2xl">
-          <Link href="/">
-            <h1 className="text-4xl font-semibold">
-              Luke<span className="text-accent">.</span>
-            </h1>
-          </Link>
-        </div>
-        {/* nav */}
-        <nav className="flex flex-col gap-8 justify-center items-center">
-          {links.map((link, index) => {
-            return (
-              <Link
-                href={link.path}
-                key={index}
-                className={`${
-                  link.path === pathname &&
-                  "text-accent border-b-2 border-accent"
-                } capitalize text-xl hover:text-accent transition-all`}
-              >
-                {link.name}
-              </Link>
-            );
-          })}
-        </nav>
+        <ScrollArea className="flex-grow">
+          {/* logo */}
+          <div className="mt-32 mb-40 text-center text-2xl">
+            <Link href="/">
+              <h1 className="text-4xl font-semibold">
+                Luke<span className="text-accent">.</span>
+              </h1>
+            </Link>
+          </div>
+          {/* nav */}
+          <nav className="flex flex-col gap-8 justify-center items-center">
+            {links.map((link, index) => {
+              return (
+                <Link
+                  href={link.path}
+                  key={index}
+                  className={`${
+                    link.path === pathname &&
+                    "text-accent border-b-2 border-accent"
+                  } capitalize text-xl hover:text-accent transition-all`}
+                >
+                  {link.name}
+                </Link>
+              );
+            })}
+          </nav>
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   );
